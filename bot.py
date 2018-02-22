70a1ace9f4b3d0c8cd0cf4c61f70e3584a554739
rnegrette <rob@negrette.me>
2018-02-22 14:56:14 -0600

    first commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@bot.command()
+async def pookie(ctx):
+    await ctx.sent("Fuck you, Pookie.")
+
+bot.run('NDE2MzAzMjEwOTAzMzcxODA4.DXCmsw.dcBMpsjlyNnoFr_tolRsx_bmPOU')
